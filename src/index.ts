9cf576384a654aaa7841edde461eea24c4751f45
Olli <otn987@gmail.com>
2020-11-19 23:14:32 +0200

    update types for pins

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,55 @@ export enum PinValue {
   "off" = "off",
 }
 
-export const pins = {
+interface Pins {
+  serialPins: {
+    gpio_14_TX: PinNumber;
+    gpio_15_RX: PinNumber;
+  };
+  i2cPins: {
+    gpio_0_eEpromData: PinNumber;
+    gpio_1_eEpromClock: PinNumber;
+    gpio_2_data: PinNumber;
+    gpio_3_clock: PinNumber;
+  };
+  hardwarePWM: {
+    gpio_12_pwm: PinNumber;
+    gpi0_13_pwm: PinNumber;
+    gpio_18_pwm: PinNumber;
+    gpio_19_pwm: PinNumber;
+  };
+  standardPins: {
+    gpio_5: PinNumber;
+    gpio_6: PinNumber;
+    gpio_16: PinNumber;
+    gpio_17: PinNumber;
+    gpio_22: PinNumber;
+    gpio_23: PinNumber;
+    gpio_24: PinNumber;
+    gpio_25: PinNumber;
+    gpio_26: PinNumber;
+    gpio_27: PinNumber;
+  };
+  spi: {
+    spi0: {
+      gpio_7_ce1: PinNumber;
+      gpio_8_ceo: PinNumber;
+      gpio_9_miso: PinNumber;
+      gpio_10_mosi: PinNumber;
+      gpio_11_sclk: PinNumber;
+    };
+    spi1: {
+      gpio_16_ce2: PinNumber;
+      gpio_17_ce1: PinNumber;
+      gpio_18_ceo: PinNumber;
+      gpio_19_miso: PinNumber;
+      gpio_20_mosi: PinNumber;
+      gpio_21_sclk: PinNumber;
+    };
+  };
+}
+
+export const pins: Pins = {
   serialPins: {
     gpio_14_TX: 14,
     gpio_15_RX: 15,
@@ -71,36 +119,37 @@ export enum EdgeType {
   "both" = "both",
 }
 
-export interface PinConfig {
-  pinNumber:
-    | 0
-    | 1
-    | 2
-    | 3
-    | 5
-    | 6
-    | 7
-    | 8
-    | 9
-    | 10
-    | 11
-    | 12
-    | 13
-    | 14
-    | 15
-    | 16
-    | 17
-    | 18
-    | 19
-    | 20
-    | 21
-    | 22
-    | 23
-    | 24
-    | 25
-    | 26
-    | 27;
+type PinNumber =
+  | 0
+  | 1
+  | 2
+  | 3
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | 13
+  | 14
+  | 15
+  | 16
+  | 17
+  | 18
+  | 19
+  | 20
+  | 21
+  | 22
+  | 23
+  | 24
+  | 25
+  | 26
+  | 27;
 
+export interface PinConfig {
+  pinNumber: PinNumber;
   type: PinType;
   edge: EdgeType;
 }
